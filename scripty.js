ce0a37d71c7b7d345d501a33a8ef57f0c1759f87
cleuber979 <105437987+cleuber979@users.noreply.github.com>
2023-11-29 03:02:30 -0300

    Add files via upload

---
+++ scripty.js
@@ -0,0 +1,15 @@
+let hero = {
+  name:"Barbaro",
+  life:100,
+  power:10,
+  defense:5
+};
+
+let orc = {
+  name:"Onyx",
+  life:120,
+  power:20,
+  defense:6
+};
+
+console.log(hero.name,orc.name);
