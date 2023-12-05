bcfd156d92538332c376952acfb7fb1c08d1d196
cleuber979 <105437987+cleuber979@users.noreply.github.com>
2023-12-05 14:10:11 -0300

    Update scripty.js
    Função atacar

--- scripty.js
+++ scripty.js
@@ -13,16 +13,26 @@ let orc = {
 };
 
 function Main () {
-  console.log("A batalha começa: ")
-  Atack()
+  console.log("A batalha começa!: ");
+  console.log("Um orc gigante apareceu!");
+  console.log("Qual a sua escolha?");
+  console.log("1 - Atacacar:");
+  console.log("2 - defender:");
+  console.log("3- Fugir!");
+
+let escolha = 1;
+
+if (escolha ==1 ) {
+  Atacar()
 }
 
-function Atack() {
-  console.log("O heroi atacou o mostro: ")
 }
 
-Main();
-
+function Atacar() {
+  console.log("O heroi ataca!");
+  console.log("O Orc predeu " + (hero.power-orc.defense));
+}
+ Main()
 
 
 
