6254754b5c823d0958e8998b35c422a60a23fe13
cleuber979 <105437987+cleuber979@users.noreply.github.com>
2023-12-08 15:20:00 -0300

    Basicos de função parte1
    Criar um repositório para avanço JS do basico ao avançado

---
+++ FunctionBasic.js
@@ -0,0 +1,28 @@
+//const pessoa1={
+   //nome:'cleuber',
+   //sobrenome:'Maciel',
+   //idade:'43'
+
+//};
+
+//console.log(pessoa1.nome);// estou entrando dentro do obejeto pessoa1 e no atributo nome
+//console.log(pessoa1.sobrenome);
+//console.log(pessoa1.idade);
+
+function criaPessoa(nome,sobrenome,idade){// função construtora esta função irá criar objetos 
+   return{
+      nome:nome,
+      sobrenome:sobrenome,
+      idade:idade
+
+ }
+}
+
+let pessoa = criaPessoa('cleuber','barbosa',45)
+let pessoa1= criaPessoa('Marina','Fonseca',38)
+
+criaPessoa('cleuber','barbosa',45)
+
+console.log(pessoa);
+console.log(pessoa1);
+
