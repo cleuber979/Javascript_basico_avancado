4fa8411cee9ac1f4bc2cf1553a80b443a8b39bd5
cleuber979 <105437987+cleuber979@users.noreply.github.com>
2023-12-08 15:33:16 -0300

    Add files via upload

---
+++ FunctionBasic2.js
@@ -0,0 +1,12 @@
+const pessoa1 = {
+   nome:'cleuber',
+   sobrenome:'Maciel',
+   idade:43,
+
+   Fala(){
+      console.log(`${this.nome} ${this.sobrenome} esta falando oi no console.log kkkkk `);// Uma função dentro de um objeto e um metodo
+
+   }
+};
+
+pessoa1.Fala(); // 
