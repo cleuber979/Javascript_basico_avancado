df473fae89bdf647ca544a5125cb89807366898a
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-07 18:34:19 -0300

    Add files via upload

---
+++ VariaveisLet.js
@@ -0,0 +1,47 @@
+// Não podemos criar variaveis com palavras reservadas 
+//Variaveis precisam ter nomes significativos 
+//Não pode começar o nome de uma variavel com um numéro
+// Não podem conter espaços e traços 
+// Utilizamos camelCase
+//case-sensitiva
+
+// demostração de uso de variaveis 
+
+let nome = 'Thiago '; // String
+// sempre utilizar let para criar variaveis 
+
+console.log(nome,"nasceu em 1984");
+console.log('Em 2000 ' + nome +' conheceu Maria');
+console.log(nome + ' casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com ' + nome +' em 2015');
+console.log('O Filho de '+ nome  ,'se chama Eduardo.'); 
+
+let nomeSecundario;// Declarou a variavel
+nomeSecundario='Pedro';// inicializei a variavel
+console.log(nomeSecundario);
+nomeSecundario='Albert'; // atribuir outro valor a variavel 
+console.log(nomeSecundario);
+
+
+//variaveis tem que ter nome significativos
+let nomeCliente ='João';
+console.log(nomeCliente);
+
+//Não podemos começar o nome da variavel com um numero
+//exemplo let 1nome; 
+
+// Não podemos conter espaços ou traços 
+//let nome Cliente ; gera erro 
+
+
+//Utilizamos cameCase
+
+let nomeCompletoDoCliente = 'Cleuber Maciel Barbosa';
+
+
+// Não podemos redeclara variaveis com let
+
+
+
+
+
