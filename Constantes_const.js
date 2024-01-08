6378fd2fda36977db1814e624584097b76ef02bf
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-07 22:22:36 -0300

    Add files via upload

---
+++ Constantes_const.js
@@ -0,0 +1,23 @@
+// Não podemos criar constatntes com palavras reservadas
+//Constantes precisam ter nomes significativos
+//Não podemos começar o nome de uma constatnte com um número
+//Não pode ter espaços ou traços
+//Utilizamos camelCase
+//Case-sensitive
+//Não pode modificar o valor de uma constatnte
+//Não UTILIZE VAR , UTILIZE const
+
+
+const nome = 'Mario';
+// nome= 'angelo';
+// Não posso atribuir outro valor a Constantes
+
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero+segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+console.log(typeof(primeiroNumero+segundoNumero));
+
+
