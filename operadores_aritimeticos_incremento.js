0fb2888bea58d93f7d8e43c0fd56863f0cf54ecf
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-02-17 03:11:24 -0300

    Add files via upload

---
+++ operadores_aritimeticos_incremento.js
@@ -0,0 +1,35 @@
+// Operadores Aritiméticos 
+// + Adição / concatenação
+// - subtração
+//  / divisão
+// * multiplicação
+// ** potenciação
+// % resto da divisão 
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1**num2);
+
+let contador = 1;
+console.log(contador++);
+
+let contador = 10;
+console.log(--contador);
+
+//NaN - Not a Number
+const numero1 = 10;
+const numero2 = 'cleuber';
+console.log(numero1*numero2)
+
+//NaN - Not a Number
+const numero1 = 10;
+const numero2 = '5';
+console.log(numero1*numero2)
+
+//NaN - Not a Number
+const numero1 = 10;
+const numero2 = parseInt('5');
+console.log(numero1*numero2);
+console.log(typeof numero2);
+
+
