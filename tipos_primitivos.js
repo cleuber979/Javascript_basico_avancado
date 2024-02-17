2056825b03f400ac073a6183faef037f2f8e831e
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-02-17 02:04:55 -0300

    Add files via upload

---
+++ tipos_primitivos.js
@@ -0,0 +1,16 @@
+// Tipos de dados primitivos 
+const nome = 'Cleuber'; //String 
+const nome1 = 'Barbosa'; //String
+const nome2 = `Maciel`; // String
+const num1 = 10; //number
+const num2 = 10.52; //number float
+let nomeAluno;//undefined não definda não aponta para lugar nenhum na menoria 
+let sobreNomeAluno= null; // nulo -> a diferença uma e do motor do javascript e o outro pelo programador 
+const aprovado = true ; // Boolean = true ,false (lógico) quer mudar o fluxo do programa 
+
+console.log(typeof nome); // quando quero saber o tipo da variavel
+
+
+ 
+
+
