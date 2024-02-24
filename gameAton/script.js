34410c5c6fac6bf04692c169ad9a7cdde57118d8
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-02-24 18:42:56 -0300

    Update script.js

--- gameAton/script.js
+++ gameAton/script.js
@@ -2,9 +2,35 @@ let canvas = document.getElementById('canvas');
 let container = canvas.getContext("2d");
 
  container.fillStyle='#8b8b8b'
- let jogador1 = container.fillRect(100,100,30,200);
- let jogador2 = container.fillRect(900,100,30,200);
- let ball = container.fillRect(960/2-15,420/2-15,30,30);
+ let jogador1 = {
+     px: 100,
+     py:100,
+     tx:30,
+     ty:200
+      }
+ 
 
- container.font = '20px Ariel';
- let Score = container.fillText("Score 1: " + 0, 200,50);
+ let jogador2 ={
+     px:900,
+     py:100,
+     tx:30,
+     ty:200
+ }
+ 
+ let ball={
+     px:960/2-15,
+     py:420/2-15,
+     tx:30,
+     ty:30
+ }
+
+
+ let pts1 = 0;
+ let pts2 = 0;
+ let Score = container.fillText("Score 1: " + pts1, 200,50);
+ let Score = container.fillText("Score 2: " + pts, 950,50);
+ 
+ function Draw(){
+     
+ };
+
