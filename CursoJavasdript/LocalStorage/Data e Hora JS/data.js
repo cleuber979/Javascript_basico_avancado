4a824ca8da0d32470ecdb9a62eb69b6ecf4c82c8
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-03-31 17:48:24 -0300

    Update data.js

--- CursoJavasdript/LocalStorage/Data e Hora JS/data.js
+++ CursoJavasdript/LocalStorage/Data e Hora JS/data.js
@@ -1,6 +1,12 @@
 
-var data = new Date();
+var data_hoje = new Date();
 
-console.log(data);
+console.log(data_hoje.getDate() );
 
-alert("olá");
+var data_de_nascimento = '1979-01-09';
+
+var ano_de_nascimento = new Date ("1979-01-09").getFullYear();
+var ano_atual = new Date().getFullyear();
+var idade = ano_atual - ano_de_nascimento;
+
+console.log(idade );
