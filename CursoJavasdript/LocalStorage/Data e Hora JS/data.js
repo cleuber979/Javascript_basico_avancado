c5962021dccd44add5082d22abf8af590decd10e
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-03-31 22:37:46 -0300

    Update data.js

--- CursoJavasdript/LocalStorage/Data e Hora JS/data.js
+++ CursoJavasdript/LocalStorage/Data e Hora JS/data.js
@@ -10,3 +10,15 @@ var ano_atual = new Date().getFullyear();
 var idade = ano_atual - ano_de_nascimento;
 
 console.log(idade );
+
+// Exercicio calculo de dias entrega
+
+var data_envio = new Date("2018-03-20");
+data_envio=data_envio.getTime();
+
+
+var data_recebimento = new Date("2018-04-06");
+data_recebimento=data_recebimento.getTime();
+
+var dias = (data_recebimento-data_envio)/86400000
+console.log(dias);
