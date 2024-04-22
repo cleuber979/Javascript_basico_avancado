c3aa77a5c2134e6e40aa11458b912c764bd20450
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-04-22 01:57:32 -0300

    Add files via upload

--- CursoJavasdript/LocalStorage/select box/scriptselectBox.js
+++ CursoJavasdript/LocalStorage/select box/scriptselectBox.js
@@ -1,12 +1,31 @@
 
+//Select box
+
 document.getElementById("mostrar_opcao").onclick=function(){
     
-   var campo_select = document.getElementById("options");
-   var indice_selecionado = campo_select.options.selectedIndex;
-   console.log(indice_selecionado)
-   var valor_selecionado = campo_select.options[indice_selecionado].innerHTML;
-   document.getElementById("opcao_selecionada").innerHTML=valor_selecionado;
+   //var campo_select = document.getElementById("options");
+   //var indice_selecionado = campo_select.options.selectedIndex;
+  // console.log(indice_selecionado)
+   //var valor_selecionado = campo_select.options[indice_selecionado].innerHTML;
+   //document.getElementById("opcao_selecionada").innerHTML=valor_selecionado;
    
-   
+   var valor_selecionado = document.getElementById("options").value;
+   document.getElementById("opcao_selecionada").innerHTML= valor_selecionado;
 
 };
+
+//Radio Buttons
+
+document.getElementById("Mostrar_radio").onclick = function() {
+
+   var radio = document.getElementsByName("genero");
+   var radio_select;
+
+   for( var a=0;a < radio.length; a++){
+      if(radio[a].checked){
+         radio_select=radio[a].value;
+         break;
+      }
+   }
+    document.getElementById("radio_selecionado").innerHTML=radio_select;
+}
