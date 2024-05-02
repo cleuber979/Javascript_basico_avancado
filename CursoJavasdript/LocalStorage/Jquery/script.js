ebaf174e4212e9d4f877562ecffb946865e9e003
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-05-02 12:53:57 -0300

    Add files via upload

--- CursoJavasdript/LocalStorage/Jquery/script.js
+++ CursoJavasdript/LocalStorage/Jquery/script.js
@@ -1,4 +1,18 @@
 
 
 var conteudo_html = $("#paragrafo-html").html();
+console.log(conteudo_html )
+
+$("#paragrafo-html").text("Conteudo novo texto escrito");
+
+
+conteudo_html = $("#paragrafo-html").html();
 console.log(conteudo_html);
+
+
+var url_link = $("#linkSite").attr("href");
+console.log(url_link)
+
+$("#linkSite").attr("href","https://github.com/cleuber979")
+url_link = $("#linkSite").attr("href");
+console.log(url_link)
