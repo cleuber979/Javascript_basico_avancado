41b4ab4ee9e2876d426459d7f733515bb72d8f77
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-05-15 03:07:54 -0300

    Add files via upload

---
+++ CursoJavasdript/LocalStorage/animate_JQuery/scripts.js
@@ -0,0 +1,14 @@
+
+
+
+$("#animar").click(function(){
+    $("#quadrado").animate({
+    width:"+=20px",
+    
+    },2000);
+
+    $("#quadrado").animate({
+        
+        height:"+=20px"
+        },2000);
+});
